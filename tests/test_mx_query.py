bd7775c94af29752928a25a2c4074d41a67e23e6
zakir <80246097+zakir-code@users.noreply.github.com>
2023-10-10 18:50:28 +0800

    add query oracle test

---
+++ tests/test_mx_query.py
@@ -0,0 +1,30 @@
+import unittest
+
+from fxsdk.client.mx_client import MxClient
+
+grpc_cli = MxClient('localhost:9090')
+
+pair_id = 'btc:cusd'
+
+
+class TestMxClientQuery(unittest.TestCase):
+
+    def test_query_oracle_price(self):
+        price = grpc_cli.query_oracle_price(pair_id)
+        print(price)
+
+    def test_query_oracle_prices(self):
+        prices = grpc_cli.query_oracle_prices()
+        print(prices)
+
+    def test_query_oracle_market(self):
+        market = grpc_cli.query_oracle_market(pair_id)
+        print(market)
+
+    def test_query_oracle_markets(self):
+        markets = grpc_cli.query_oracle_markets()
+        print(markets)
+
+
+if __name__ == '__main__':
+    unittest.main()
