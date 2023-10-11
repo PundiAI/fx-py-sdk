7442119fbe2d2698a6aeeb8d8b702112f215bd05
zakir <80246097+zakir-code@users.noreply.github.com>
2023-10-11 17:13:42 +0800

    add height params to marginx client

--- fxsdk/client/marginx.py
+++ fxsdk/client/marginx.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
+from typing import Optional
 
-from fxsdk.client.grpc import Client
+from fxsdk.client.grpc import Client, GRPCBlockHeightHeader
 from fxsdk.dec import dec_from_str
 from fxsdk.msg.marginx import new_position_from_proto, new_order_from_proto, Order, Position, \
     new_pair_funding_rate_from_proto, PairFundingRate, OrderDepths, PairPrice, new_order_depths_from_proto
@@ -20,69 +21,83 @@ class MarginXClient(Client):
     def __init__(self, url: str = 'localhost:9090'):
         super().__init__(url)
 
-    def query_oracle_price(self, pair_id: str) -> Decimal:
-        response = OracleClient(self.channel).Price(QueryPriceRequest(pair_id=pair_id))
+    def query_oracle_price(self, pair_id: str, height: Optional[int] = 0) -> Decimal:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = OracleClient(self.channel).Price(QueryPriceRequest(pair_id=pair_id), metadata=metadata)
         return dec_from_str(response.price)
 
-    def query_oracle_prices(self) -> [PairPrice]:
-        response = OracleClient(self.channel).Prices(QueryPriceRequest())
+    def query_oracle_prices(self, height: Optional[int] = 0) -> [PairPrice]:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = OracleClient(self.channel).Prices(QueryPriceRequest(), metadata=metadata)
         pair_prices = []
         for price in response.prices:
             pair_prices.append(PairPrice(price.pair_id, dec_from_str(price.price)))
         return pair_prices
 
-    def query_oracle_market(self, pair_id: str) -> Market:
-        response = OracleClient(self.channel).Market(QueryMarketRequest(pair_id=pair_id))
+    def query_oracle_market(self, pair_id: str, height: Optional[int] = 0) -> Market:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = OracleClient(self.channel).Market(QueryMarketRequest(pair_id=pair_id), metadata=metadata)
         return response.market
 
-    def query_oracle_markets(self) -> [Market]:
-        response = OracleClient(self.channel).Markets(QueryMarketRequest())
+    def query_oracle_markets(self, height: Optional[int] = 0) -> [Market]:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = OracleClient(self.channel).Markets(QueryMarketRequest(), metadata=metadata)
         return response.markets
 
-    def query_positions(self, owner: str, pair_id: str) -> [Position]:
+    def query_positions(self, owner: str, pair_id: str, height: Optional[int] = 0) -> [Position]:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = DexClient(self.channel).QueryPosition(QueryPositionReq(owner=owner, pair_id=pair_id),
+                                                         metadata=metadata)
         positions = []
-        response = DexClient(self.channel).QueryPosition(QueryPositionReq(owner=owner, pair_id=pair_id))
         for position in response.positions:
             positions.append(new_position_from_proto(position))
         return positions
 
-    def query_order(self, order_id: str) -> Order:
-        response = DexClient(self.channel).QueryOrder(QueryOrderRequest(order_id=order_id))
+    def query_order(self, order_id: str, height: Optional[int] = 0) -> Order:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = DexClient(self.channel).QueryOrder(QueryOrderRequest(order_id=order_id), metadata=metadata)
         order = new_order_from_proto(response.order)
         return order
 
-    def query_orders(self, owner: str, pair_id: str) -> [Order]:
+    def query_orders(self, owner: str, pair_id: str, height: Optional[int] = 0) -> [Order]:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = DexClient(self.channel).QueryOrders(QueryOrdersRequest(owner=owner, pair_id=pair_id),
+                                                       metadata=metadata)
         orders = []
-        response = DexClient(self.channel).QueryOrders(QueryOrdersRequest(owner=owner, pair_id=pair_id))
         for order in response.orders:
             orders.append(new_order_from_proto(order))
         return orders
 
-    def query_funding_info(self) -> Funding:
-        response = DexClient(self.channel).QueryFunding(QueryFundingReq())
+    def query_funding_info(self, height: Optional[int] = 0) -> Funding:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = DexClient(self.channel).QueryFunding(QueryFundingReq(), metadata=metadata)
         return response.funding
 
-    def query_funding_rates(self, last_or_realtime: bool = True) -> [PairFundingRate]:
+    def query_funding_rates(self, last_or_realtime: bool = True, height: Optional[int] = 0) -> [PairFundingRate]:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
         response = DexClient(self.channel).QueryPairFundingRates(
-            QueryPairFundingRatesReq(last_or_realtime=last_or_realtime))
+            QueryPairFundingRatesReq(last_or_realtime=last_or_realtime), metadata=metadata)
         pair_funding_rates = []
         for rate in response.pair_funding_rates:
             pair_funding_rates.append(new_pair_funding_rate_from_proto(rate))
         return pair_funding_rates
 
-    def query_order_depths(self, pair_id: str) -> OrderDepths:
-        response = DexClient(self.channel).QueryOrderbook(QueryOrderbookReq(pair_id=pair_id))
+    def query_order_depths(self, pair_id: str, height: Optional[int] = 0) -> OrderDepths:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = DexClient(self.channel).QueryOrderbook(QueryOrderbookReq(pair_id=pair_id), metadata=metadata)
         return new_order_depths_from_proto(response.Asks, response.Bids)
 
-    def query_mark_price(self, paid_id: str) -> Decimal:
-        response = DexClient(self.channel).QueryMarkPrice(QueryMarkPriceReq(pair_id=paid_id))
+    def query_mark_price(self, paid_id: str, height: Optional[int] = 0) -> Decimal:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = DexClient(self.channel).QueryMarkPrice(QueryMarkPriceReq(pair_id=paid_id), metadata=metadata)
         if len(response.pair_mark_price) > 0:
             return dec_from_str(response.pair_mark_price[0].price)
         else:
             raise Exception("No mark price found for pair_id: {}".format(paid_id))
 
-    def query_mark_prices(self) -> [PairPrice]:
-        response = DexClient(self.channel).QueryMarkPrice(QueryMarkPriceReq(query_all=True))
+    def query_mark_prices(self, height: Optional[int] = 0) -> [PairPrice]:
+        metadata = [(GRPCBlockHeightHeader, str(height))]
+        response = DexClient(self.channel).QueryMarkPrice(QueryMarkPriceReq(query_all=True), metadata=metadata)
         pair_prices = []
         for price in response.pair_mark_price:
             pair_prices.append(PairPrice(price.pair_id, dec_from_str(price.price)))
