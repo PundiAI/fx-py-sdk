0489e068c9da24d2696e73ec68d9e7a4d5750176
zakir <80246097+zakir-code@users.noreply.github.com>
2023-10-11 14:37:24 +0800

    update version to v0.6.8

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setup(
     name="fx-py-sdk",
-    version="0.6.7",
+    version="0.6.8",
     platforms='any',
     description="Python library for FunctionX",
     long_description=long_description,
