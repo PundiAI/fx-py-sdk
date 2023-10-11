697dd3c10023fa288eec8c45f75ff2101d86826b
zakir <80246097+zakir-code@users.noreply.github.com>
2023-10-11 16:15:38 +0800

    add ibc fx msg transfer

--- fxsdk/msg/__init__.py
+++ fxsdk/msg/__init__.py
@@ -11,6 +11,14 @@ from fxsdk.x.fx.dex.v1.order_pb2 import Direction
 from fxsdk.x.fx.dex.v1.tx_pb2 import MsgCreateOrder, MsgCancelOrder, MsgClosePosition, MsgAddMargin
 from fxsdk.x.ibc.applications.transfer.v1.tx_pb2 import MsgTransfer
 from fxsdk.x.ibc.core.client.v1.client_pb2 import Height
+from fxsdk.x.fx.ibc.applications.transfer.v1.tx_pb2 import MsgTransfer as FxMsgTransfer
+
+
+def calculate_future_timestamp(seconds: int) -> int:
+    dtime = datetime.datetime.now()
+    ans_time = time.mktime(dtime.timetuple())
+    ans_time = (int(ans_time) + seconds) * 1000 * 1000 * 1000
+    return ans_time
 
 
 def new_msg_send(from_address: str, to_address: str, amount: [Coin]) -> Any:
@@ -27,22 +35,41 @@ def new_msg_send(from_address: str, to_address: str, amount: [Coin]) -> Any:
 
 
 def new_ibc_msg_transfer(channel: str, sender: str, receiver: str, token: Coin, memo: str) -> Any:
-    height = Height(revision_number=0, revision_height=0)
-    dtime = datetime.datetime.now()
-    ans_time = time.mktime(dtime.timetuple())
-    ans_time = (int(ans_time) + 60 * 60) * 1000 * 1000 * 1000  # 1 hour
     msg = MsgTransfer(
         source_port="transfer",
         source_channel=channel,
         token=token,
         sender=sender,
         receiver=receiver,
-        timeout_height=height,
-        timeout_timestamp=ans_time,
+        timeout_height=Height(revision_number=0, revision_height=0),
+        timeout_timestamp=calculate_future_timestamp(60 * 60),  # 1 hour
         memo=memo,
     )
     msg_any = Any(
         type_url='/ibc.applications.transfer.v1.MsgTransfer',
+        value=msg.SerializeToString(),
+    )
+    return msg_any
+
+
+def new_ibc_fx_msg_transfer(channel: str, sender: str, receiver: str, token: Coin, router: str, fee: Coin,
+                            memo: str) -> Any:
+    if token.denom != fee.denom:
+        raise Exception("token denom must equal to fee denom")
+    msg = FxMsgTransfer(
+        source_port="transfer",
+        source_channel=channel,
+        token=token,
+        sender=sender,
+        receiver=receiver,
+        timeout_height=Height(revision_number=0, revision_height=0),
+        timeout_timestamp=calculate_future_timestamp(60 * 60),  # 1 hour
+        router=router,
+        fee=fee,
+        memo=memo,
+    )
+    msg_any = Any(
+        type_url='/fx.ibc.applications.transfer.v1.MsgTransfer',
         value=msg.SerializeToString(),
     )
     return msg_any
